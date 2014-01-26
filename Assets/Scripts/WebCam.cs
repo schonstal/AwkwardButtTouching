d693ed91c631c7926e8bd448e48cf1ddbdbed97e
Josh Schonstal <josh@schonstal.org>
2014-01-26 09:35:47 -0800

    Finally have something worth committing

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+	public int cameraNumber = 0;
+
+	private WebCamTexture webcamTexture;
+  private double scaleY;
+
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length > cameraNumber) {
+			webcamTexture = new WebCamTexture(devices[cameraNumber].name, 640, 480, 30);
+			renderer.material.mainTexture = webcamTexture;
+			webcamTexture.Play();
+      Debug.Log("Successfully added camera " + cameraNumber);
+		} else {
+			Debug.Log("no camera");
+		}
+	}
+}
